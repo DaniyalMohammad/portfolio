9ca58b93bdcb12830f633ad106f379fd0e72e262
Syed <emailmeonzeeshan@gmail.com>
2022-08-14 11:45:37 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import { useEffect } from "react";
+import "./App.css";
+
+function App() {
+  useEffect(() => {
+    const script = document.createElement("script");
+
+    script.src = "https://platform.linkedin.com/badges/js/profile.js";
+    script.async = true;
+    script.defer = true;
+
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
+  return (
+    <div
+      className="badge-base LI-profile-badge"
+      data-locale="en_US"
+      data-size="medium"
+      data-theme="dark"
+      data-type="VERTICAL"
+      data-vanity="daniyal-mohammad"
+      data-version="v1"
+    >
+      <a
+        className="badge-base__link LI-simple-link"
+        href="https://in.linkedin.com/in/daniyal-mohammad?trk=profile-badge"
+      ></a>
+    </div>
+  );
+}
+
+export default App;
